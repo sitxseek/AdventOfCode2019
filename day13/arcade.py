5cd193a0994ca16416000a73216577f19a5cbeb9
Yang Guo <yangzong58@gmail.com>
2019-12-22 11:37:51 -0700

    day13 part 1

---
+++ day13/arcade.py
@@ -0,0 +1,118 @@
+def extend(data, n):
+    if n >= len(data):
+        data += [0] * (n - len(data) + 1)
+
+f = open("input.txt", "r")
+data = list(map(int, f.read().split(",")))
+
+relative_base = 0
+i = 0
+sequence = 0
+block_tiles = 0
+while i < len(data):
+    instr = str(data[i])
+    if instr[-1:] == '3':
+        if instr[0] == '2':
+            extend(data, relative_base + data[i + 1])
+            data[relative_base + data[i + 1]] = 2
+        else:
+            extend(data, data[i + 1])
+            data[data[i + 1]] = 2
+        i += 2
+    elif instr[-1:] == '4':
+        sequence += 1
+        if instr[0] == '1':
+            print("output", i + 1)
+            val = data[i + 1]
+            # if data[i + 1] != 0:
+            #     raise ValueError('A test failed')
+        elif instr[0] == '2':
+            extend(data, relative_base + data[i + 1])
+            print("output", relative_base + data[i + 1])
+            val = data[relative_base + data[i + 1]]
+            # if data[relative_base] != 0:
+            #     raise ValueError('A test failed')
+        else:
+            extend(data, data[i + 1])
+            print("output", data[i + 1])
+            val = data[data[i + 1]]
+            # if data[data[i + 1]] != 0:
+            #     raise ValueError('A test failed')
+
+        if sequence == 3:
+            sequence = 0
+            if val == 2:
+                block_tiles += 1
+        i += 2
+    elif instr == '99':
+        break
+    elif instr[-1:] == '9':
+        if instr[0] == '1':
+            relative_base += data[i + 1]
+        elif instr[0] == '2':
+            extend(data, relative_base + data[i + 1])
+            relative_base += data[relative_base + data[i + 1]]
+        else:
+            extend(data, data[i + 1])
+            relative_base += data[data[i + 1]]
+        i += 2
+    else:
+        count = 4
+        first = second = 0
+        extend(data, data[i + 3])
+        third = data[i + 3]
+        if len(instr) == 1:
+            extend(data, data[i + 1])
+            extend(data, data[i + 2])
+            first = data[data[i + 1]]
+            second = data[data[i + 2]]
+        elif len(instr) == 3:
+            if instr[0] == '1':
+                first = data[i + 1]
+            else:
+                extend(data, relative_base + data[i + 1])
+                first = data[relative_base + data[i + 1]]
+            extend(data, data[i + 2])
+            second = data[data[i + 2]]
+        else:
+            if instr[-4] == '1': second = data[i + 2]
+            elif instr[-4] == '0': 
+                extend(data, data[i + 2])
+                second = data[data[i + 2]]
+            else:
+                extend(data, relative_base + data[i + 2]) 
+                second = data[relative_base + data[i + 2]]
+            if instr[-3] == '0': 
+                extend(data, data[i + 1])
+                first = data[data[i + 1]]
+            elif instr[-3] == '1': first = data[i + 1]
+            else: 
+                extend(data, relative_base + data[i + 1])
+                first = data[relative_base + data[i + 1]]
+            if (len(instr) == 5):
+                extend(data, relative_base + data[i + 3])
+                third = relative_base + data[i + 3]
+
+        if instr[-1:] == '1': 
+            data[third] = first + second
+        elif instr[-1:] == '2': 
+            data[third] = first * second
+        elif instr[-1:] == '5': 
+            if first != 0:
+                i = second
+                count = 0
+            else:
+                count = 3
+        elif instr[-1:] == '6': 
+            if first == 0:
+                i = second
+                count = 0
+            else:
+                count = 3
+        elif instr[-1:] == '7':
+            data[third] = 1 if first < second else 0
+        elif instr[-1:] == '8':
+            data[third] = 1 if first == second else 0
+        i += count
+
+print("block tiles:", block_tiles)
